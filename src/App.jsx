1e9ac77f853e1d6ff72930bb3d2bb562c4a4e903
Homura-san <erickbispo@ufba.br>
2025-02-19 07:58:13 -0300

    Estilizando Card #3

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,18 @@ import Card from './components/layouts/Card';
 export default _ =>
     <div className="App">
         <div className="Cards">
-            <Card titulo="Componente com Parâmetro">
+            <Card titulo="Desafio Aleatório" color="#FA6900">
+                <Aleatorio min={1} max={60} />
+            </Card>
+
+            <Card titulo="Componente com Parâmetro" color="#E94C6F">
                 <ComParametro
                     titulo="Situação do Aluno"
                     aluno="Carlos Pareira" nota={6.2}
                 />
             </Card>
 
-            <Card titulo="Desafio Aleatório">
-                <Aleatorio min={1} max={60} />
-            </Card>
-
-            <Card titulo="Primeiro Componente">
+            <Card titulo="Primeiro Componente" color="#E8B71A"> 
                 <Primeiro />
             </Card>
         </div>
--- src/components/layouts/Card.jsx
+++ src/components/layouts/Card.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import "./Card.css";
 
 export default (props) => {
+    const cardStyle = {
+        backgroundColor: props.color || '#F00',
+        borderColor: props.color || '#F00'
+    }
+    
     return (
         <div className="Card">
-            <div className="Title">{props.titulo}</div>
+            <div className="Title" style={cardStyle}>{props.titulo}</div>
             <div className="Content">
                 {props.children}
             </div>
