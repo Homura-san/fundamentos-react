ed1112a8c5bf78ff0efcac137f78832a31f2b9e8
Homura-san <erickbispo@ufba.br>
2025-09-29 11:43:39 -0300

    Desafio Mega Sena - Tentativa

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,16 @@ import DiretaPai from "./components/comunicacao/DiretaPai";
 import IndiretaPai from "./components/comunicacao/IndiretaPai";
 import Input from "./components/formulario/Input";
 import Contador from "./components/contador/Contador";
+import Mega from "./components/mega/Mega";
 
 // Sintaxe de função mais reduzida possível
 
 export default _ =>
     <div className="App">
+        <Card titulo="#13 - Desafio Mega" color="#424242">
+            <Mega></Mega>
+        </Card>
+
         <Card titulo="#12 - Contador" color="#424242">
             <Contador numeroInicial={10}></Contador>
         </Card>
